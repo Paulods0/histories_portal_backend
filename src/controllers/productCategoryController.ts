128b891244accdfa5c9a20b163efaa8d7f2d9cd1
Paulods0 <pauloluguenda0@gmail.com>
2024-03-15 17:01:28 +0100

    feat: create product-category controller

--- src/controllers/productCategoryController.ts
+++ src/controllers/productCategoryController.ts
@@ -1,10 +1,59 @@
 import { Request, Response } from "express"
+import { productCategoryModel } from "../models/productCategoryModel"
 
-const createProductCategory = async (req: Request, res: Response) => {}
-const getAllProductCategories = async (req: Request, res: Response) => {}
-const getProductCategoryById = async (req: Request, res: Response) => {}
-const updateProductCategory = async (req: Request, res: Response) => {}
-const deleteProductCategory = async (req: Request, res: Response) => {}
+const createProductCategory = async (req: Request, res: Response) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ message: "O nome da categoria é obrigatório" })
+  }
+  try {
+    const productCategory = new productCategoryModel({ name })
+    await productCategory.save()
+    res
+      .status(201)
+      .json({ message: "Categoria criada com sucesso", data: productCategory })
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro no servidor: " + error,
+    })
+  }
+}
+
+const getAllProductCategories = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro no servidor: " + error,
+    })
+  }
+}
+const getProductCategoryById = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro no servidor: " + error,
+    })
+  }
+}
+const updateProductCategory = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro no servidor: " + error,
+    })
+  }
+}
+const deleteProductCategory = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro no servidor: " + error,
+    })
+  }
+}
 
 export {
   createProductCategory,
